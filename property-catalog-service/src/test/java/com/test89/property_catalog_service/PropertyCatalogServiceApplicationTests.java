edbb2be152c92786a664653dd941194ceb13d9e9
Vijayamaharaj <5148113+vijayamaharaja@users.noreply.github.com>
2025-03-24 15:45:07 +0000

    Unused imports cleared

--- property-catalog-service/src/test/java/com/test89/property_catalog_service/PropertyCatalogServiceApplicationTests.java
+++ property-catalog-service/src/test/java/com/test89/property_catalog_service/PropertyCatalogServiceApplicationTests.java
@@ -1,9 +1,7 @@
 package com.test89.property_catalog_service;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
 
 @SpringBootTest
 //@ImportAutoConfiguration(exclude = {
